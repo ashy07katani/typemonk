d927ac684fe29cf8dc7f531e743a11a37a227efe
ashy07katani <tripathi.ashish29@gmail.com>
2023-01-16 16:27:21 +0530

    issue still not solved

--- src/components/TypeArea.js
+++ src/components/TypeArea.js
@@ -139,69 +139,6 @@ export default function TypeArea(props) {
             };
           });
     }
-
-    // setTestInfo((preVal) => {
-    //   if (event.keyCode === 32) {
-    //     if (matchFound(formedCurWord, wordList[wordIndex])) {
-    //       setFormedCurWord("");
-    //       // setTotalKeysPressed((preVal)=>{return preVal+1});
-    //       setTestInfo((preVal) => {
-    //         return {
-    //           ...preVal,
-    //           totalKeysPressed: preVal["totalKeysPressed"] + 1,
-    //         };
-    //       });
-    //       setwordIndex((preVal) => {
-    //         return preVal + 1;
-    //       });
-    //       curLetter = " ";
-    //       console.log("space pressed: ", formedCurWord);
-    //       console.log("space pressed: ", preVal["paragraphFormed"]);
-    //       return {
-    //         ...preVal,
-    //         paragraphFormed: preVal["paragraphFormed"].concat(curLetter),
-    //       };
-    //     }
-    //   } else if (event.keyCode === 8) {
-    //     console.log("backspace pressed");
-    //     setFormedCurWord((preVal) => {
-    //       return preVal.slice(0, -1);
-    //     });
-    //     curLetter = "";
-    //     if (formedCurWord !== "") {
-    //       return {
-    //         ...preVal,
-    //         paragraphFormed: preVal["paragraphFormed"].slice(0, -1),
-    //       };
-    //     } else {
-    //       console.log("backspace pressed: ", formedCurWord);
-    //     }
-    //   } else if (allowedKey.includes(event.keyCode)) {
-    //     console.log("you know a valid key is pressed");
-    //     setTestInfo((preVal) => {
-    //       return {
-    //         ...preVal,
-    //         totalKeysPressed: preVal["totalKeysPressed"] + 1,
-    //       };
-    //     });
-    //     setTotalKeysPressed((preVal) => {
-    //       return preVal + 1;
-    //     });
-    //     curLetter = event.key;
-    //     setFormedCurWord((preVal) => {
-    //       return preVal.concat(curLetter);
-    //     });
-    //     return {
-    //       ...preVal,
-    //       paragraphFormed: preVal["paragraphFormed"].concat(curLetter),
-    //     };
-    //   }
-
-    //   return {
-    //     ...preVal,
-    //     paragraphFormed: preVal["paragraphFormed"].concat(curLetter),
-    //   };
-    // });
   }
 
   return (
